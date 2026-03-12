ec0a3db2e750a41e6f919656fb4f917910a80a3e
Anup <anupdey9863@gmail.com>
2026-03-12 16:41:03 +0530

    fix deployment error 4

--- src/app/admin/dashboard/franchise/page.jsx
+++ src/app/admin/dashboard/franchise/page.jsx
@@ -1,5 +1,5 @@
-'use client'
 export const dynamic = "force-dynamic";
+'use client'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { account, databases } from '@/lib/appwrite'
