66dc8ca3757fae758977136721e7d29eb2dfe50a
Anup <anupdey9863@gmail.com>
2026-06-15 12:31:54 +0530

    beauty certi update

--- src/app/login/institute/(protected)/certificate/beauty-certificate/[id]/page.jsx
+++ src/app/login/institute/(protected)/certificate/beauty-certificate/[id]/page.jsx
@@ -536,7 +536,7 @@ const franchiseSign =
       >
 
         {/* TEMPLATE */}
-        <img src="/beautycerti.png" className="absolute w-full h-full" />
+        <img src="/certi.jpg.jpeg" className="absolute w-full h-full" />
 
        
         {/* LOGO */}
