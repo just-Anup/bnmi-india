f41fd6ace3126d4bcbced7318d6fa2618e078d34
Anup <anupdey9863@gmail.com>
2026-06-17 12:59:28 +0530

    certificate update

--- src/app/login/institute/(protected)/certificate/multiple-certificate/[id]/page.jsx
+++ src/app/login/institute/(protected)/certificate/multiple-certificate/[id]/page.jsx
@@ -559,7 +559,7 @@ useEffect(() => {
       >
 
         {/* TEMPLATE */}
-        <img src="/singlemark.png" className="absolute w-full h-full" />
+        <img src="/certificate.png" className="absolute w-full h-full" />
 
         {/* LOGO */}
       {student.logo && (
