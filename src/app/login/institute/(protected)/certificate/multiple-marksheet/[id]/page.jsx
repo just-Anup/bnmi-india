038b1ef1603bf5ef3c6de8d8b1647a5c9f308cba
Anup <anupdey9863@gmail.com>
2026-06-26 10:48:31 +0530

    multiple mark logo round

--- src/app/login/institute/(protected)/certificate/multiple-marksheet/[id]/page.jsx
+++ src/app/login/institute/(protected)/certificate/multiple-marksheet/[id]/page.jsx
@@ -290,15 +290,17 @@ const totalOutOf = marksArray.length * 100;
         <img src="/multiplemarksheet.png" className="absolute w-full h-full" />
 
         {/* LOGO */}
-        {student?.logo && (
-          <img
-  id="logo-img"
-  src={student.logo + "&mode=admin"}  // 🔥 ADD THIS
-  crossOrigin="anonymous"
-  className="absolute top-[10px] left-[400px] w-[120px] h-[120px]"
-/>
+               {student.logo && (
+  <div className="absolute top-[10px] left-[410px] w-[135px] h-[135px] overflow-hidden bg-white rounded-full border-4 border-white flex items-center justify-center shadow-md">
+    <img
+      src={student.logo}
+      className="w-full h-full object-cover rounded-full"
+    />
+  </div>
 
-        )}
+)}
+
+  
 
         {/* ===============================
             LEFT SIDE
